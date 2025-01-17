a3153baa6153057370810e1d08c105941f0ea879
Sreejan Chowdhury <4500sreejanchowdhury@gmail.com>
2025-01-17 06:22:06 +0530

    #1 - Created Demo01.razor - This displays the contacts as they are added by the user from the UI using the "Add Contact" btn.

---
+++ BlazorServerAppDemo/ContactService.cs
@@ -0,0 +1,15 @@
+﻿using BlazorServerAppDemo.Models;
+using System.Collections.Generic;
+
+namespace BlazorServerAppDemo
+{
+    public class ContactService
+    {
+        public List<Contact> Contacts = new List<Contact>();
+        public void AddContact(Contact contact)
+        {
+            Contacts.Add(contact);
+        }
+
+    }
+}
--- BlazorServerAppDemo/Startup.cs
+++ BlazorServerAppDemo/Startup.cs
@@ -23,6 +23,7 @@ namespace BlazorServerAppDemo
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+            services.AddSingleton<ContactService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
