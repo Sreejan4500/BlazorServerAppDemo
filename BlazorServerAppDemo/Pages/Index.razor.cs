857afaf0e8d3a98ded9ea0228143967fa648279b
Sreejan Chowdhury <4500sreejanchowdhury@gmail.com>
2025-01-17 05:33:42 +0530

    #10 - Splitting C# code, HTML code & css code - We added new files of .cs, .css to store the respective code type. For index.razor, we need to create index.razor.cs and index.razor.css

---
+++ BlazorServerAppDemo/Pages/Index.razor.cs
@@ -0,0 +1,38 @@
+﻿using BlazorServerAppDemo.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BlazorServerAppDemo.Pages
+{
+    public partial class Index
+    {
+        private List<Contact> contacts;
+
+        private Dictionary<string, object> MyTextAttributes = new() {
+            {"placeholder", "Placeholder 03"},
+            {"type", "number"}
+        };
+
+        protected async override Task OnInitializedAsync()
+        {
+            await Task.Delay(5000);
+
+            contacts = new List<Contact>
+            {
+                new() {
+                    FirstName = "Sreejan",
+                    LastName = "Chowdhury",
+                    Email = "sc@gmail.com"
+                },
+                new() {
+                    FirstName = "Akash",
+                    LastName = "Derasree",
+                    Email = "skyad@gmail.com"
+                }
+            };
+
+            await base.OnInitializedAsync();
+
+        }
+    }
+}
