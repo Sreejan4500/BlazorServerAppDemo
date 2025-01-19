84ccc6d5c41ce57ccc401dcc77f46b5d86b9ab50
Sreejan Chowdhury <4500sreejanchowdhury@gmail.com>
2025-01-19 06:38:36 +0530

    #5 - Calling instance C# method from JS. (The prev was static C# method).

--- BlazorServerAppDemo/wwwroot/js/sample.js
+++ BlazorServerAppDemo/wwwroot/js/sample.js
@@ -5,3 +5,10 @@
 			alert("Message from Method: " + result);
 		});
 }
+
+function callInstanceMethod(instanceObject) {
+	instanceObject.invokeMethodAsync("GetValueFromInstance")
+		.then(result => {
+			alert("Message from Method:" + result);
+		});
+}
